285166bb4c5eb1dfa309bb327c4b42b3b5f05f9a
Pantaiz <pantaiz@yandex.ru>
2023-06-14 23:27:44 +0300

    redux-form

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -1,7 +1,11 @@
 import {useAppDispatch} from "../../../app/hooks";
 import {authThunks} from "../auth.slice";
 import s from './styles.module.css'
-
+import {useForm} from "react-hook-form";
+//useref
+//gпрнимание общего работы браузера
+//сборка приложения , конфиги
+//js оптимизация
 export const Login = () => {
     const dispatch = useAppDispatch();
     const loginHandler = () => {
@@ -12,11 +16,17 @@ export const Login = () => {
         }
         dispatch(authThunks.login(payload));
     };
-
+const {register,handleSubmit}=useForm()
     return (
         <div className={s.container}>
             <h1>Login</h1>
-            <button onClick={loginHandler}>login</button>
+            <form  onSubmit={handleSubmit((data)=>{
+                console.log(data)
+            })}>
+                <input {...register(("login"),{ required:true,minLength:4}) } placeholder={'login'}/>
+                <input {...register(("password"))} placeholder={'password'}/>
+                <button type={"submit"}> Submit </button>
+            </form>
         </div>
     );
 };
